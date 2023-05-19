65d9f9082403714d65947ba1e10a2d56518e4dc6
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-05-20 02:16:58 +0300

    Update RestaurantOwneServices.java

--- enterprise-java-project/src/main/java/com/project/services/RestaurantOwneServices.java
+++ enterprise-java-project/src/main/java/com/project/services/RestaurantOwneServices.java
@@ -1,25 +1,5 @@
 package com.project.services;
 
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@Stateless
-public class RestaurantOwnerService {
-    @PersistenceContext
-    private EntityManager em;
-
-    // Create restaurant menu
-    // Edit restaurant: change menu meals for each restaurant
-    // Get restaurant details by id
-    // Create restaurant report: given a restaurant id print
-    // how much the restaurant earns (summation of total amount of all completed
-    // orders) , Number of completed orders, Number of canceled orders
-    
-}
-
-package com.project.services;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.HeuristicMixedException;
