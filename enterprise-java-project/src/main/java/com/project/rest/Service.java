4e33f4c28c4e7b913c08755870eb8995c7d5280c
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-05-20 16:40:56 +0300

    Create Service.java

---
+++ enterprise-java-project/src/main/java/com/project/rest/Service.java
@@ -0,0 +1,10 @@
+package com.project.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/api")
+public class Service extends Application {
+
+}
