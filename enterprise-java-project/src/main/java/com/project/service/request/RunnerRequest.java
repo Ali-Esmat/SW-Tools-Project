a61990dfd11f4048bc0a6a36f8ab52256f750f0e
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-05-22 00:33:44 +0300

    Update RunnerRequest.java

--- enterprise-java-project/src/main/java/com/project/service/request/RunnerRequest.java
+++ enterprise-java-project/src/main/java/com/project/service/request/RunnerRequest.java
@@ -1,7 +1,10 @@
 package com.project.service.request;
 
+import com.advancedtools.project.enums.RoleEnum;
+
 public class RunnerRequest extends UserRequest {
     private double deliveryFees;
+    private RoleEnum status;
 
     public double getDeliveryFees() {
         return deliveryFees;
@@ -9,5 +12,13 @@ public class RunnerRequest extends UserRequest {
 
     public void setDeliveryFees(double deliveryFees) {
         this.deliveryFees = deliveryFees;
+    }
+    
+    public RoleEnum getRunnerStatus() {
+        return status;
+    }
+
+    public void setRunnerStatus(RoleEnum status) {
+        this.status = status;
     }
 }
